b594394ca49ef5376b6184b65f773ac65fec8002
bai-yuwei <yw_bai@outlook.com>
2025-09-07 17:14:46 +0800

    [feat]: windows demo程序编译成功

--- config/windows/windows.cmake
+++ config/windows/windows.cmake
@@ -5,12 +5,12 @@ set(CROSS_SUFFIX "")
 
 set(CMAKE_C_COMPILER "${CROSS_PREFIX}gcc${CROSS_SUFFIX}")
 set(CMAKE_CXX_COMPILER "${CROSS_PREFIX}g++${CROSS_SUFFIX}")
-set(CMAKE_ASM_COMPILER "${CROSS_PREFIX}nasm${CROSS_SUFFIX}")
+set(CMAKE_ASM_COMPILER "${CROSS_PREFIX}as${CROSS_SUFFIX}")
 
 set(
     CFLAGS
     -m32
-    -nostdilib
+    -nostdlib
     -nostdinc
     -fno-builtin
     -fno-stack-protector
@@ -20,7 +20,7 @@ set(
 
 set(
     LFLAGS
-    -m elf_i386
+    -m32
 )
 
 set(
